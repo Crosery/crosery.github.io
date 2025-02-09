30dfb6248c3bd6f694026afbf33a001621ea24d4
Crosery <2022003007@yangtzeu.edu.cn>
2025-02-09 19:54:13 +0800

    Site updated: 2025-02-09 19:54:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/09/30/ROBOT/Q群云崽使用教程/","2024/07/31/开源项目/ROS双线巡线方案/","2025/01/31/galgame汉化/第一次汉化记录/"];function toRandomPost(){
+var posts=["2025/01/31/galgame汉化/第一次汉化记录/","2024/07/31/开源项目/ROS双线巡线方案/","2023/09/30/ROBOT/Q群云崽使用教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
